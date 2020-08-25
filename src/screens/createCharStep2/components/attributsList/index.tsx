1b5cb266dedc8c873a4457ad8e987d1542580e41
Nycollas <nycollas.duaarte@hotmail.com>
2020-08-25 01:34:55 -0300

    add redux structure

--- src/screens/createCharStep2/components/attributsList/index.tsx
+++ src/screens/createCharStep2/components/attributsList/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { Store } from '../../../../redux/store';
 
 import Attributes from '../../../../components/attributes';
 
