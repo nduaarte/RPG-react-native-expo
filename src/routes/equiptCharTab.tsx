a504572308077658f5fa4f0e3d5bdb3f9ae790f5
Nycollas <nycollas.duaarte@hotmail.com>
2020-09-01 02:43:40 -0300

    update

--- src/routes/equiptCharTab.tsx
+++ src/routes/equiptCharTab.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import StatsIcon from '../assets/icons/attributes.png';
+import statsIcon from '../assets/icons/attributes.png';
+import inventoryIcon from '../assets/icons/inventoryIcon.png';
 
 import Stats from '../screens/stats';
 import Inventory from '../screens/inventory';
@@ -13,13 +15,20 @@ export default function InventoryTab() {
   return(
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name='Stats' component={Stats} options={{
+        <Tab.Screen name='Estatísticas' component={Stats} options={{
           tabBarIcon: () => {
-            let icon = StatsIcon
-            icon
+            return(
+              <Image source={statsIcon} style={{width: 30, height: 30}} />
+            );
           }
-        }}/>
-        <Tab.Screen name='Inventory' component={Inventory} />
+        }} />
+        <Tab.Screen name='Inventário' component={Inventory} options={{
+          tabBarIcon: () => {
+            return(
+              <Image source={inventoryIcon} style={{width: 30, height: 30}} />
+            );
+          }
+        }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
