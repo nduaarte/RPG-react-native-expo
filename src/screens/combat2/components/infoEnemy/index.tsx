eb6587b012cc8ca6d18fceb5af0e005acf2af6e2
Nycollas <nycollas.duaarte@hotmail.com>
2020-09-12 22:33:20 -0300

    'update'

--- src/screens/combat2/components/infoEnemy/index.tsx
+++ src/screens/combat2/components/infoEnemy/index.tsx
@@ -31,7 +31,7 @@ export default function InfoEnemy() {
   //     || 
   //     enemies[chosenEnemy].biome == 'todos'
   //   );    
-  // }, []);
+  // }, [])
 
   return(
     <View style={styles.container}>
