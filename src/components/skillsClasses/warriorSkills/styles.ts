2462e83a2bbd6a756d0678f8028cdf9b27c4a8d8
Nycollas <nycollas.duaarte@hotmail.com>
2020-10-12 01:49:55 -0300

    'update'

---
+++ src/components/skillsClasses/warriorSkills/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+
+  buttonHability: {
+    backgroundColor: '#de2840',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 10,
+    width: 150,
+    borderWidth: 1.5,
+    borderColor: '#000'
+  },
+
+  icon: {
+    width: 40,
+    height: 40
+  }
+});
+
+export default styles;
